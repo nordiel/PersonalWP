18ce02234e694da1ca70f61c16e2d8e590d10830
nxrdiel <nordielm@gmail.com>
2023-09-03 22:09:59 -0400

    Added script to change about me messages

---
+++ script2.js
@@ -0,0 +1,27 @@
+const greetings = ["Technology Enthusiast", "Pet Friendly", "Python Enjoyer", "JavaScript Enjoyer", "C# Hater"]; // array of greetings
+
+let currentIndex = 0;
+
+function changeGreeting() {
+
+  const spanElement = document.getElementById("who-i-am");
+
+  // Fade out the greeting text
+  spanElement.style.opacity = 0;
+
+  setTimeout(() => {
+    // get greeting at the current index
+    spanElement.textContent = greetings[currentIndex];
+
+    // fading greeting text
+    spanElement.style.opacity = 1;
+
+    //incrementing the current index
+    currentIndex = (currentIndex + 1) % greetings.length;
+  }, 500); // delaying the text update to allow for fade-out effect
+}
+
+changeGreeting();
+
+// calling the changeGreeting() function every 4 seconds
+setInterval(changeGreeting, 4000);
